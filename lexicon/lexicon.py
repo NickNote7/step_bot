00a44c034a93daa5662632936390b4ecfba70733
NotMan <notionmanman@gmail.com>
2024-09-12 10:56:34 +0300

    Хендлеры, лексикон

---
+++ lexicon/lexicon.py
@@ -0,0 +1,6 @@
+
+LEXICON_RU: dict[str, str] = {
+    '/start': 'HELLO!\n\n"THIS START',
+    '/help': 'HELLO!\n\n"GOT A COPY OF UR MESSAGE',
+    'no_echo': 'FUCK YOU, ASSHOLE!'
+}
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-from aiogram import Dispatcher
+import asyncio
+import logging
+
+from aiogram import Dispatcher, Bot
+from config_data.config import load_config
+
+async def start() -> None:
+    config = load_config()
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s')
+
+    BOT = Bot(token=config.tg_bot.token)
+    BOT_DISPATCHER = Dispatcher()
+
+    await BOT.delete_webhook(drop_pending_updates=True)
+    await BOT_DISPATCHER.start_polling(BOT)
+
+if __name__ == "__main__":
+    asyncio.run(start())
+
